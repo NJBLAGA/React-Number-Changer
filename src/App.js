901f9c560e04a1d7728100f1ff40befb150c95c6
NJBLAGA <nathanblaga90@gmail.com>
2021-06-21 20:59:14 +1000

    Created simple Number Changer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 
 function App() {
+  const ACTIONS = {
+    INCREMENT: "increment",
+    DECREMENT: "decrement",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case ACTIONS.INCREMENT:
+        return { count: state.count + 1 };
+      case ACTIONS.DECREMENT:
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  // const [count, setCount] = useState(0);
+
+  function increment() {
+    // setCount((prevCount) => prevCount + 1);
+    dispatch({ type: ACTIONS.INCREMENT });
+  }
+
+  function decrement() {
+    // setCount((prevCount) => prevCount - 1);
+    dispatch({ type: ACTIONS.DECREMENT });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Changer">
+        <h1 className="Title">Number Changer</h1>
+        <button className="Button" onClick={decrement}>
+          -
+        </button>
+        <span className="Number">{state.count}</span>
+        <button className="Button" onClick={increment}>
+          +
+        </button>
+      </div>
+    </>
   );
 }
 
